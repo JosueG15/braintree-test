54af7b0cc4a71bf7196aac9091f5cfb8b4d8f73c
JosueG15 <garciajosue402@gmail.com>
2021-05-05 16:59:42 -0600

    Modifying cors

--- server.js
+++ server.js
@@ -7,12 +7,9 @@ const cors = require('cors');
 
 const port = process.env.PORT || 3001;
 const app = express();
-var corsOptions = {
-  origin: '*',
-};
 
 app.use(morgan('dev'));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
