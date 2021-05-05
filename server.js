6b3868e90b92cc30b7278a3c507b133e7708d3fa
JosueG15 <garciajosue402@gmail.com>
2021-05-05 16:58:00 -0600

    Adding all origin to cors options

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const cors = require('cors');
 
 const port = process.env.PORT || 3001;
 const app = express();
+var corsOptions = {
+  origin: '*',
+};
 
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
